f29eb99f450a0c77616175413ca4b949a0ca78aa
Francesca F <fodfran@gmail.com>
2019-09-24 00:52:48 -0400

    Update HelloController.cs

--- HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/Controllers/HelloController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,23 +12,43 @@ namespace HelloMVC.Controllers
         public IActionResult Index()
         {
             string html = "<form method='post'> " +
-                "<input type='text' name='name' />" +
+                "<input type='text' name='name'/>" +
                 "<select name = 'language'>" +
-                "< option value = 'Hello' > English </ option >" +
-                "< option value = 'Hola' > Spanish </ option >" +
-                "< option value = 'Bonjour' > French </ option >" +
-                "< option value = '今日は' > Japanese </ option >" +
-                "< option value = '안녕하세요' > Korean </ option >" +
-                "</ select > " +
+                "<option value = 'Hello' > English </option>" +
+                "<option value = 'Hola' > Spanish </option>" +
+                "<option value = 'Bonjour' > French </option>" +
+                "<option value = 'Konnichiwa' > Japanese </option>" +
+                "<option value = 'Annyeonghaseyo' > Korean </option>" +
+                "</select> " +
                 "<input type='submit' value='Greet me!' />" +
                 "</form>";
             return Content(html, "text/html");
             //return Redirect("/Hello/Goodbye");
         }
 
+        static int counter;
         [Route("/Hello")]
         [HttpPost]
-        public IActionResult Display(string name = "SHINee")
+        public IActionResult CreateMessage(string name, string language)
+        {
+            
+            if (Request.Cookies["counter"] == null)
+            {
+                counter = 1;
+            }
+            else
+            {
+                counter = int.Parse(Request.Cookies["counter"]);
+                counter++;
+            }
+            Response.Cookies.Append("counter", counter.ToString());
+
+            return Content(string.Format("<h1>{0} {1}</h1>\nGreetings: {2}", language, name, counter.ToString()), "text/html");
+        }
+        
+        //[Route("/Hello")]
+        //[HttpPost]
+        public IActionResult Display(string name)
         {
             /*if (string.IsNullOrEmpty(name))
             {
@@ -46,7 +67,7 @@ namespace HelloMVC.Controllers
 
         // /Hello/Goodbye
         // alter the route to this controller to be: /Hello/Aloha
-        //[Route("/Hello/Aloha")]
+        [Route("/Hello/Aloha")]
         public IActionResult Goodbye()
         {
             return Content("Goodbye");
