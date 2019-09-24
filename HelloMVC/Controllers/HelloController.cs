46080f462dd8b7f6b187a16ca0fe9109a2574da2
Francesca F <fodfran@gmail.com>
2019-09-23 20:11:18 -0400

    pre-work done

--- HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/Controllers/HelloController.cs
@@ -7,9 +7,42 @@ namespace HelloMVC.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            string html = "<form method='post'> " +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "SHINee")
+        {
+            /*if (string.IsNullOrEmpty(name))
+            {
+                name = "SHINee";
+            }*/
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        // Handle request to /Hello/NAME (URL segment)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+
+        }
+
+
+        // /Hello/Goodbye
+        // alter the route to this controller to be: /Hello/Aloha
+        //[Route("/Hello/Aloha")]
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
         }
     }
 }
